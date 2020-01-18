ddd705019a31ff615bf5c03b20b72a0affb03603
Hiroharu Hara <hh.25799623@gmail.com>
2020-01-18 13:30:58 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <div><h2>Hello World halhal!</h2></div>
-  }
+// class App extends Component {
+//   render() {
+//     return <div><h2>Hello World halhal!!!!!</h2></div>
+//   }
+// }
+const App = () => {
+  const profiles = [
+    { name: 'hiroharu', age: 22 },
+    { name: 'tarou', age: 37 },
+    { name: 'yamada', age: 32 },
+    { name: 'takashi' },
+    { age: 43 }
+  ]
+
+  return (
+    <React.Fragment>
+      {
+        profiles.map((profile) => {
+          return <User name={profile.name} age={profile.age} />
+        })
+      }
+    </React.Fragment>
+  )
 }
+
+const User = (props) => {
+return <h1>I am { props.name } and my old is { props.age } !</h1>
+}
+
+User.defaultProps = {
+  name: 'NoName',
+  age: 0
+}
+
 
 export default App;
